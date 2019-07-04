69a94db5819a7040d38d296cd1d6f97849d204f9
Alex Mirrington <alexmirrington@gmail.com>
2019-07-04 16:07:16 +1000

    Store items in local storage

--- todo/index.js
+++ todo/index.js
@@ -1,9 +1,12 @@
 class ItemList {
 
-    constructor(root) {
+    constructor(root, persist=true) {
         this.root = root
         this.items = [];
+        this.persist = persist;
         this.idCursor = 0;
+
+        this.load();
     }
 
     addItem(text) {
@@ -15,10 +18,11 @@ class ItemList {
         let itemNode = document.createElement('div');
         itemNode.className = 'list-item';
 
-        let itemText = document.createTextNode(text);
+        let itemText = document.createElement('p');
+        itemText.innerText = text;
 
         let itemRemoveButton = document.createElement('button');
-        itemRemoveButton.innerText = 'Remove'
+        itemRemoveButton.innerText = 'Remove';
 
         itemNode.appendChild(itemText);
         itemNode.appendChild(itemRemoveButton);
@@ -30,26 +34,47 @@ class ItemList {
             id: this.idCursor,
             elem: itemNode
         }
-        console.log(addedItem);
-        console.log(this.items);
 
         this.items.push(addedItem);
 
         // Register callbacks
         itemRemoveButton.onclick = event => this.removeItem(addedItem);
 
+        if (this.persist) {
+            this.save();
+        }
+
         this.idCursor++;
     }
 
     removeItem(item) {
-        console.log(item);
-        console.log(this.items);
         let idx = this.items.findIndex(i => item.id === i.id);
         if (idx === -1) {
             return;
         }
         this.items.splice(idx, 1);
         item.elem.remove();
+
+        if (this.persist) {
+            this.save();
+        }
+    }
+
+    save() {
+        let toSave = this.items.map(item => {
+            return {
+                value: item.elem.querySelector('p').innerText
+            };
+        });
+        localStorage['items'] = JSON.stringify(toSave);
+    }
+
+    load() {
+        // TODO: Handle uninitialised localStorage and errors.
+        let toLoad = JSON.parse(localStorage['items']);
+        toLoad.forEach(element => {
+            this.addItem(element.value);
+        });
     }
 
 }
